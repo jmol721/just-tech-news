11da8e7e9fd7358484267c982ca08a19eae4e4c3
jose <jmolina721@gmail.com>
2021-06-30 21:30:23 -0500

    Added dashboard logic to edit and delete posts

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 router.use('/', homeRoutes);
 
+const dashboardRoutes = require('./dashboard-routes.js');
+router.use('/dashboard', dashboardRoutes);
+
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
